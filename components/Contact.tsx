e14a55d35e4b2646c6793f17da96410dc368273f
Ottokingstedt <otto.kingstedt@gmail.com>
2024-11-04 17:35:35 +0100

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -52,7 +52,7 @@ const Contact = () => {
   return (
     <section className="px-5 pt-12 lg:px-0 lg:pt-0 ">
       <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-300 dark:text-white">
+        <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-300 ">
           Under Konstruktion
         </h2>
         <p className="mb-8 lg:mb-16 text-center text-gray-300">
@@ -65,7 +65,7 @@ const Contact = () => {
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="name@address.com"
               required
               value={email}
@@ -77,7 +77,7 @@ const Contact = () => {
             <input
               type="text"
               id="subject"
-              className="block p-3 w-full text-sm text-gray-400 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block p-3 w-full text-sm text-gray-200 rounded-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-600  shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:placeholder-gray-400  dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="Titeln"
               required
               value={subject}
@@ -89,7 +89,7 @@ const Contact = () => {
             <textarea
               id="message"
               rows={6}
-              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block p-2.5 w-full text-sm text-gray-200 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Hej Otto..."
               required
               value={message}
