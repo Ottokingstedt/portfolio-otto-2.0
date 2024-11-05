c8a247941b26c488e00b20798ec93dc7d61bbe27
Ottokingstedt <otto.kingstedt@gmail.com>
2024-11-05 13:16:24 +0100

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -61,11 +61,11 @@ const Contact = () => {
         </p>
         <form onSubmit={handleSubmitForm} className="space-y-8">
           <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-300">Din epost</label>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium dark:text-gray-300">Din epost</label>
             <input
               type="email"
               id="email"
-              className="shadow-sm bg-gray-50 border border-gray-300 text-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="shadow-sm bg-gray-50 border text-black border-gray-300 dark:text-gray-300 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="name@address.com"
               required
               value={email}
@@ -73,11 +73,11 @@ const Contact = () => {
             />
           </div>
           <div>
-            <label htmlFor="subject" className="block mb-2 text-sm font-medium text-gray-300 ">Titeln</label>
+            <label htmlFor="subject" className="block mb-2 text-sm font-medium dark:text-gray-300 ">Titeln</label>
             <input
               type="text"
               id="subject"
-              className="block p-3 w-full text-sm text-gray-200 rounded-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-600  shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:placeholder-gray-400  dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+              className="block text-black p-3 w-full text-sm dark:text-gray-200 rounded-lg border border-gray-300 dark:bg-gray-700 dark:border-gray-600  shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:placeholder-gray-400  dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
               placeholder="Titeln"
               required
               value={subject}
@@ -85,11 +85,11 @@ const Contact = () => {
             />
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-400">Meddela</label>
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-300">Meddela</label>
             <textarea
               id="message"
               rows={6}
-              className="block p-2.5 w-full text-sm text-gray-200 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block p-2.5 w-full text-sm text-black dark:text-gray-200 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Hej Otto..."
               required
               value={message}
